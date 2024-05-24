1474d22ecac3c5c900c0b0792d3d14948dc0f549
Jan Heiland <jnhlnd@gmail.com>
2024-05-24 09:19:15 +0200

    link fix

--- mkdc.sh
+++ mkdc.sh
@@ -4,4 +4,4 @@
 Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::pdf_book", quiet=FALSE)'
 Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook", quiet=FALSE)'
 
-cp docs/NdML.pdf /home/heiland/clouds/nc-tui/teaching/24-ndml/
+cp docs/NdML.pdf ~/clouds/nc-tui/teaching/24-ndml/
